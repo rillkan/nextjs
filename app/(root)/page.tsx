e4f765407602ee8cf10568bc4c3285b0a0f61c50
Reagan Kan <mjkan96@gmail.com>
2025-02-18 16:47:58 +0800

    Added a post card

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,20 @@
+import StartupCard from "@/components/StartupCard";
 import SearchForm from "../../components/SearchForm";
 
 export default async function Home({ searchParams }: { searchParams: Promise<{ query?: string }> }) {
   const query = (await searchParams).query
+
+  const posts = [{
+    _createdAt: "Yesterday",
+    views: 55,
+    autheor: { _id: 1 },
+    _id: 1,
+    description: "This is a description",
+    image: "https://placehold.co/600x400",
+    category: "Robots",
+    title: "We Robots"
+
+  }]
   return (
     <>
       <section className="pink_container">
@@ -10,7 +23,21 @@ export default async function Home({ searchParams }: { searchParams: Promise<{ q
         <p className="sub-heading !max-w-3xl">Submit Ideas, Vote on Pitches, and Get Notcied in Virtual Competitions</p>
         <SearchForm query={query} />
       </section>
-      <h1 className="heading">Home</h1>
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search results for "${query}"` : "All Startups"}
+        </p>
+
+        <ul className="mt-7 card_grid">
+          {posts?.length > 0 ? (
+            posts.map((post: StartupCardType) => (
+              <StartupCard key={posts?._id} post={post} />
+            ))
+          ) : (
+            <p className="no-results">No startups found</p>
+          )}
+        </ul>
+      </section>
     </>
   )
 }
