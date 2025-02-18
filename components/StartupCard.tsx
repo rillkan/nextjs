42ee5a90c695dd99697cda26c583a1aae4da661b
Reagan Kan <mjkan96@gmail.com>
2025-02-18 17:08:29 +0800

    Adding Image using placeholder and granting permissions from next.config

--- components/StartupCard.tsx
+++ components/StartupCard.tsx
@@ -1,5 +1,6 @@
 import { formatDate } from "@/lib/utils"
 import { EyeIcon } from "lucide-react"
+import Image from "next/image"
 import Link from "next/link"
 
 const StartupCard = ({ post }: { post: StartupTypeCard }) => {
@@ -19,8 +20,15 @@ const StartupCard = ({ post }: { post: StartupTypeCard }) => {
 
       <div>
         <div className="flex-1">
-          <Link href={`/user/${authorId}`} />
-          <p className="text-16-medium line-clamp-1">{name}</p>
+          <Link href={`/user/${authorId}`}>
+            <p className="text-16-medium line-clamp-1">{name}</p>
+          </Link>
+          <Link href={`/startup/${_id}`}>
+            <h3 className="">{title}</h3>
+          </Link>
+          <Link href={`/user/${authorId}`}>
+            <Image src="https://placehold.co/48x48" alt="placeholder" width={48} height={48} className="rounded-full" />
+          </Link>
         </div>
       </div>
     </li>
