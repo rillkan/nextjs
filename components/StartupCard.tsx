5ecab53030ac92627776b3fca619d8ef5d05ae34
Reagan Kan <mjkan96@gmail.com>
2025-02-18 17:15:54 +0800

    Complete body of a StartupCard

--- components/StartupCard.tsx
+++ components/StartupCard.tsx
@@ -2,10 +2,12 @@ import { formatDate } from "@/lib/utils"
 import { EyeIcon } from "lucide-react"
 import Image from "next/image"
 import Link from "next/link"
+import { Button } from "./ui/button"
+
 
 const StartupCard = ({ post }: { post: StartupTypeCard }) => {
 
-  const { _createdAt, views, author: { _id: authorId, name }, title, category, _id, image } = post
+  const { _createdAt, views, author: { _id: authorId, name }, title, category, _id, image, description } = post
   return (
     <li className="startup-card group">
       <div className="flex-between">
@@ -30,6 +32,23 @@ const StartupCard = ({ post }: { post: StartupTypeCard }) => {
             <Image src="https://placehold.co/48x48" alt="placeholder" width={48} height={48} className="rounded-full" />
           </Link>
         </div>
+      </div>
+      <Link href={`/startup/${_id}`}>
+        <p className="startup-card_desc">
+          {description}
+        </p>
+
+        <img src={image} alt="placeholder" className="startup-card_img" />
+      </Link>
+
+      <div className="flex-between gap-3 mt-5">
+        <Link href={`/?query=${category.toLowerCase()}`}>
+          <p>{category}</p>
+        </Link>
+        <Button className="startip-card-btn" asChild>
+          <Link href={`/startup/${_id}`}>
+          </Link>
+        </Button>
       </div>
     </li>
   )
