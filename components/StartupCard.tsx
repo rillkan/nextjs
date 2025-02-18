54fcf52676bf4870e6219801e36c800aca15af8e
Reagan Kan <mjkan96@gmail.com>
2025-02-18 16:53:40 +0800

    Refactor StartupCard

--- components/StartupCard.tsx
+++ components/StartupCard.tsx
@@ -3,22 +3,24 @@ import { EyeIcon } from "lucide-react"
 import Link from "next/link"
 
 const StartupCard = ({ post }: { post: StartupTypeCard }) => {
+
+  const { _createdAt, views, author: { _id: authorId, name }, title, category, _id, image } = post
   return (
     <li className="startup-card group">
       <div className="flex-between">
         <p className="startup_card_date">
-          {formatDate(post._createdAt)}
+          {formatDate(_createdAt)}
         </p>
         <div>
           <EyeIcon />
-          <span></span>
+          <span>{views}</span>
         </div>
       </div>
 
       <div>
         <div className="flex-1">
-          <Link href={`/user/${post.author?._id}`} />
-          <p className="text-16-medium line-clamp-1">{post.author?.name}</p>
+          <Link href={`/user/${authorId}`} />
+          <p className="text-16-medium line-clamp-1">{name}</p>
         </div>
       </div>
     </li>
