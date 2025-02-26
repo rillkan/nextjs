67cd607e34a9563d213caf43d899341722f95180
Reagan Kan <mjkan96@gmail.com>
2025-02-26 16:33:54 +0800

    feat(auth): add GitHub OAuth with automatic author creation in Sanit

--- sanity/queries.ts
+++ sanity/queries.ts
@@ -37,3 +37,15 @@ export const STARTUP_VIEWS_QUERY = defineQuery(`
   views
   }
   `)
+
+export const AUTHOR_BY_GITHUB_ID_QUERY = defineQuery(`
+    *[_type == "author" && _id == $id][0]{
+    _id,
+    id,
+    name,
+    username,
+    email,
+    image,
+    bio
+    }
+    `)
